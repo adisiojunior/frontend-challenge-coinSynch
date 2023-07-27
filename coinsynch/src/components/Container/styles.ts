bec1585088d6b134b21775abfddd1c670d5f0f40
Adiso Fialho Jr <adisio.junior@bycoders.com.br>
2023-07-27 09:58:33 -0300

    Criação do cabeçario e ajustes de pastas

---
+++ coinsynch/src/components/Container/styles.ts
@@ -0,0 +1,12 @@
+
+import styled, { css } from 'styled-components'
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    max-width: ${theme.grid.container};
+    width: 100%;
+    padding-left: 7rem;
+    padding-right: 7rem;
+    margin: 0 auto;
+  `}
+`;
---
+++ coinsynch/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import HeaderNavigation from "@/components/HeaderNavigation"
+
+
+export default function Home() {
+  return (
+    <>
+      <HeaderNavigation />
+      <h1>Hello World!</h1>
+    </>
+  )
+}
